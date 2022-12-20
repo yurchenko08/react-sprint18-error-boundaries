a54507ece5bbbf3e05ca7b877bdae1dca7325c0f
yurchenko08 <yurchenko08@gmail.com>
2022-12-20 18:55:03 +0200

    added handleError

--- src/components/DataReceiver.js
+++ src/components/DataReceiver.js
@@ -1,14 +1,25 @@
 import { useState } from 'react';
 import { getData } from '../api';
-import { ErrorBoundary } from 'react-error-boundary';
+import { ErrorBoundary, useErrorHandler } from 'react-error-boundary';
 import ErrorFallback from './ErrorFallback';
 
 function DataReceiver() {
   const [data, setData] = useState(0);
+  const handleError = useErrorHandler();
   return (
     <ErrorBoundary FallbackComponent={ErrorFallback}>
       <div className='data'>
-        <button onClick={() => setData(getData())}>Get new data</button>
+        <button
+          onClick={() => {
+            try {
+              setData(getData());
+            } catch (e) {
+              handleError(e);
+            }
+          }}
+        >
+          Get new data
+        </button>
         <div>{data}</div>
       </div>
     </ErrorBoundary>
